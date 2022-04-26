1548b501ff2816437ec6bf1b623d21b59d2923c1
Clobes Araújo <ClobesA1@verifone.com>
2022-04-26 03:05:58 -0300

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { DataState } from './enum/data-state.enum';
+import { AppState } from './interface/app-state';
+import { FizzBuzzResponse } from './interface/fizzbuzz';
+import { FizzbuzzService } from './service/fizzbuzz.service';
+
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fizzbuzzfrontend';
+  appState$: Observable<AppState<FizzBuzzResponse>>;
+  readonly DataState = DataState;
+
+  checkoutForm = this.formBuilder.group({
+    min: '',
+    max: ''
+  });
+
+  constructor(private fizzBuzzService: FizzbuzzService,
+              private formBuilder: FormBuilder,){}
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    // Process checkout data here
+    const min: number = this.checkoutForm.value.min;
+    const max: number = this.checkoutForm.value.max;
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.fizzBuzzService.sendFizzBuzz(min, max)
+    .subscribe(
+      data => successAlertBox(data),
+      error => console.error('Error!', error)
+    )
+    this.checkoutForm.reset();
+  }
 }
+async function successAlertBox(data: FizzBuzzResponse) {
+  Swal.fire({
+    title: 'Response',
+    html:
+    '<h4>Code: </h4> <p>'+data.code+'</p>' +
+    '<h4>Timestamp: </h4> <p>'+data.timestamp+'</p>' +
+    '<h4>Descripción: </h4> <p>'+data.description+'</p>' +
+    '<h4>List: </h4><p>'+data.list+'</p>',
+    focusConfirm: false,
+  })
+  /*
+  Swal.fire('Code: '+data.code +"\n" +
+   "Timestamp: " + data.timestamp + "\n" +
+   "Description: " + data.description + "\n" +
+   "List: "+data.list)
+   */
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TopBarComponent } from './component/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule ,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
